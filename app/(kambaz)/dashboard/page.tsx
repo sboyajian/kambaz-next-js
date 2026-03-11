c29c2de803cc293de4d08fadbfb72aae4d3d83b1
sboyajian <sophia.boyajian@gmail.com>
2026-03-11 16:27:16 -0400

    Pls work

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -56,6 +56,7 @@ export default function Dashboard() {
         onChange={(e) => setCourse({ ...course, name: e.target.value })}
       />
       <FormControl
+        as="textarea"
         value={course.description}
         rows={3}
         onChange={(e) => setCourse({ ...course, description: e.target.value })}
