ecb4d8dd6a2cbcb9ae47b58efbeb1aa477fc6023
sboyajian <sophia.boyajian@gmail.com>
2026-03-11 16:31:34 -0400

    end me

--- app/labs/lab4/redux/todos/TodoItem.tsx
+++ app/labs/lab4/redux/todos/TodoItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 import { Button, ListGroupItem } from "react-bootstrap";
-export default function TodoItem({ todo }) {
+export default function TodoItem({ todo }: { todo: any }) {
   const dispatch = useDispatch();
   return (
     <ListGroupItem key={todo.id}>
