eb5b932b904516a6d57d3fe46cd080d439725d59
sboyajian <sophia.boyajian@gmail.com>
2026-03-29 16:58:14 -0400

    fix

--- app/labs/lab5/WorkingWithObjectsAsynchronously.tsx
+++ app/labs/lab5/WorkingWithObjectsAsynchronously.tsx
@@ -28,6 +28,7 @@ export default function WorkingWithObjectsAsynchronously() {
         }
       />
       <FormControl
+        as="textarea"
         rows={3}
         defaultValue={assignment.description}
         className="mb-2"
